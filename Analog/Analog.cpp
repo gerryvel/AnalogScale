b1d2ff8540db4a8433a0bab2c1f184f6a72b210c
Gerry <gerry@sebb.de>
2020-11-29 00:08:09 +0100

    Update Analog.cpp

--- Analog/Analog.cpp
+++ Analog/Analog.cpp
@@ -1,8 +1,10 @@
 /*
+Analog.cpp
+
 Analog input scaling V1.0
 
 Input OGR  / UGR : Values from Input Device e.g. 0-4096
-Output OGR / UGR : Values physical e.g. 0-150°C
+Output OGR / UGR : Values physical e.g. 0-150Â°C
 
 G. Sebb, 24.10.2020
 
